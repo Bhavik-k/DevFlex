cf9123f9a67362f5613d4ebf85908ffe281425cd
Bhavik Khawale <62886971+Bhavik-k@users.noreply.github.com>
2021-11-24 23:42:38 -0500

    Animations Added

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,21 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade';
 
 function Header() {
     return (
         <div className='flex py-3 px-5'>
             {/* <img className="h-12"
              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmg6VbQr7k4bE8m1sGjODK19nEZn-UKVChBg&usqp=CAU"/> */}
-            <img className="md:h-20 h-14"
-             src="Logo.jpg"/>
-            <h1 className="md:ml-5 text-3xl font-semibold my-auto ml-3">DevFlex</h1>
+            <Fade left>
+                <img className="md:h-20 h-14"
+                src="Logo.jpg"/>
+                <h1 className="md:ml-5 text-3xl font-semibold my-auto ml-3">DevFlex</h1>
+            </Fade>
+            <Fade right>
             <div className="ml-auto my-auto px-5 h-10 flex justify-center items-center border-2 border-gray-400 rounded-full shadow-md hover:scale-105 hover:shadow-lg duration-200 cursor-pointer">
                 <h1 className="">Try now</h1>
             </div>
+            </Fade>
         </div>
     )
 }
--- pages/pages.js
+++ pages/pages.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Head from 'next/head'
 import Header from '../components/Header'
 import Project from '../components/Project';
+import Bounce from 'react-reveal/Bounce';
+import Zoom from 'react-reveal/Zoom';
+import Flip from 'react-reveal/Flip';
 
 export default function pages() {
     return (
@@ -14,34 +17,48 @@ export default function pages() {
             <div className="grid place-content-center">
                 <div className="flex mt-3 justify-center items-center w-screen">
                     <div className="flex-1 border-2 border-gray-200 h-0"></div>
-                    <img className="h-40 border-2 border-gray-200 rounded-full" src="https://avatars.dicebear.com/api/micah/Naaahrendra ahawale.svg"/>
+                    <Flip><img className="h-40 border-2 border-gray-200 rounded-full" src="https://avatars.dicebear.com/api/micah/Naaahrendra ahawale.svg"/></Flip>
                     <div className="flex-1 border-2 border-gray-200 h-0"></div>
                 </div>
                 <h1 className="mx-auto mt-3 text-4xl font-semibold font">Bhavik Khawale</h1>
                 {/* Contact Icons */}
                 <div className="flex mx-auto my-3">
-                    <img className="w-10 mx-2 cursor-pointer hover:scale-105 duration-200" src="\imges\svgs\facebook-brands.svg"/>
-                    <img className="w-10 mx-2 cursor-pointer hover:scale-105 duration-200" src="\imges\svgs\github-brands.svg"/>
-                    <img className="w-10 mx-2 cursor-pointer hover:scale-105 duration-200" src="\imges\svgs\instagram-brands.svg"/>
-                    <img className="w-10 mx-2 cursor-pointer hover:scale-105 duration-200" src="\imges\svgs\linkedin-brands.svg"/>
-                    <img className="w-10 mx-2 cursor-pointer hover:scale-105 duration-200" src="\imges\svgs\twitter-brands.svg"/>
+                    <Zoom><img className="w-10 mx-2 cursor-pointer hover:scale-105 duration-200" src="\imges\svgs\facebook-brands.svg"/></Zoom>
+                    <Zoom><img className="w-10 mx-2 cursor-pointer hover:scale-105 duration-200" src="\imges\svgs\github-brands.svg"/></Zoom>
+                    <Zoom><img className="w-10 mx-2 cursor-pointer hover:scale-105 duration-200" src="\imges\svgs\instagram-brands.svg"/></Zoom>
+                    <Zoom><img className="w-10 mx-2 cursor-pointer hover:scale-105 duration-200" src="\imges\svgs\linkedin-brands.svg"/></Zoom>
+                    <Zoom><img className="w-10 mx-2 cursor-pointer hover:scale-105 duration-200" src="\imges\svgs\twitter-brands.svg"/></Zoom>
                 </div>
                 <div>
-                    <Project
-                    img="https://bhavik-k.github.io/CV/img/works/bnb.png"
-                    name="Airbnb Clone"
-                    discription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.Lorem ipsum dolor sit ametipsum dolor sit amet,"
-                    link="https://airbnb-kohl.vercel.app/"/>
-                    <Project
-                    img="https://bhavik-k.github.io/CV/img/works/bnb.png"
-                    name="Airbnb Clone"
-                    discription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.Lorem ipsum dolor sit amet,ipsum dolor sit amet"
-                    link="https://airbnb-kohl.vercel.app/"/>
-                    <Project
-                    img="https://bhavik-k.github.io/CV/img/works/bnb.png"
-                    name="Airbnb Clone"
-                    discription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.Lorem ipsum dolor sit ametipsum dolor sit amet,"
-                    link="https://airbnb-kohl.vercel.app/"/>
+                    <Bounce left>
+                        <Project
+                        img="https://bhavik-k.github.io/CV/img/works/bnb.png"
+                        name="Airbnb Clone"
+                        discription="Web designers generally have nothing to do with creating content for their projects. Even so, the look of a site can be incomplete if no words are included. Web designers need to find ways to "
+                        link="https://airbnb-kohl.vercel.app/"/>
+                    </Bounce>
+                    <Bounce left>
+                        <Project
+                        img="https://bhavik-k.github.io/CV/img/works/bnb.png"
+                        name="Airbnb Clone"
+                        discription="Web designers generally have nothing to do with creating content for their projects. Even so, the look of a site can be incomplete if no words are included. Web designers need to find ways to "
+                        link="https://airbnb-kohl.vercel.app/"/>
+                    </Bounce>
+                    <Bounce left>
+                        <Project
+                        img="https://bhavik-k.github.io/CV/img/works/bnb.png"
+                        name="Airbnb Clone"
+                        discription="Web designers generally have nothing to do with creating content for their projects. Even so, the look of a site can be incomplete if no words are included. Web designers need to find ways to "
+                        link="https://airbnb-kohl.vercel.app/"/>
+                    </Bounce>
+                    <Bounce left>
+                        <Project
+                        img="https://bhavik-k.github.io/CV/img/works/bnb.png"
+                        name="Airbnb Clone"
+                        discription="Web designers generally have nothing to do with creating content for their projects. Even so, the look of a site can be incomplete if no words are included. Web designers need to find ways to "
+                        link="https://airbnb-kohl.vercel.app/"/>
+                    </Bounce>
+                    
                 </div>
             </div>
         </div>
